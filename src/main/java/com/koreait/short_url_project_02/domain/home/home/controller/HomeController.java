1fc62fb3495e4dfafa81921ce03c9894f02c078a
biso15 <biso150@gmail.com>
2025-02-24 03:43:21 +0900

    배포테스트

--- src/main/java/com/koreait/short_url_project_02/domain/home/home/controller/HomeController.java
+++ src/main/java/com/koreait/short_url_project_02/domain/home/home/controller/HomeController.java
@@ -17,7 +17,7 @@ public class HomeController {
     @GetMapping("/")
     @ResponseBody
     public String home() {
-        return "1234Main Page!!! " + customSiteName;
+        return "1234567Main Page!!! " + customSiteName;
     }
 
     @GetMapping("/secretKey")
